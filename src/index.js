e601ec343f98094df3f7c796cc4a53a153a553ae
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-12-09 18:55:34 +0100

    task start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './css/styles.css';
+var debounce = require('lodash.debounce');
+
+const refs = {
+    inputQuery: document.querySelector('#search-box'),
+};
 
 const DEBOUNCE_DELAY = 300;
+const handleInput = (event) => {
+    event.preventDefault();
+        const query = event.target.value;
+        console.log(query);
+        fetchCountries(query)
+            .then(renderCountry)
+            .catch(error => console.log(error));
+};
+
+refs.inputQuery.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
+
+
+
+const fetchCountries = (query) => {
+    return fetch(`https://restcountries.com/v3.1/name/${query}`)
+        .then(r => r.json());
+};
+
+const renderCountry = (countriesList) => {
+
+    console.log(countriesList);
+    console.log(countriesList.length);
+    if (countriesList.length === 1) {
+        console.log('One country ');
+    } else if (countriesList.length > 1 && countriesList.length <= 10) {
+        console.log('2 - 10 countries ');
+    } else if (countriesList.length > 10) {
+        console.log('too much ');
+    };
+};
