f939cd86c36c0c0293cb0deef622f76d1f3a6634
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-12-10 13:49:09 +0100

    task part 2

--- src/index.js
+++ src/index.js
@@ -3,36 +3,67 @@ var debounce = require('lodash.debounce');
 
 const refs = {
     inputQuery: document.querySelector('#search-box'),
+    countriesList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
 };
 
 const DEBOUNCE_DELAY = 300;
 const handleInput = (event) => {
     event.preventDefault();
-        const query = event.target.value;
+        const query = event.target.value.trim();
         console.log(query);
         fetchCountries(query)
-            .then(renderCountry)
+            .then(catchCountry)
             .catch(error => console.log(error));
 };
 
 refs.inputQuery.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 
-
 const fetchCountries = (query) => {
-    return fetch(`https://restcountries.com/v3.1/name/${query}`)
+    return fetch(`https://restcountries.com/v3.1/name/${query}?fields=name,capital,population,flags,languages`)
         .then(r => r.json());
 };
 
-const renderCountry = (countriesList) => {
+const catchCountry = (countriesList) => {
 
     console.log(countriesList);
     console.log(countriesList.length);
+
     if (countriesList.length === 1) {
-        console.log('One country ');
+
+        // const languagesObj = Object.values(countriesList[0].languages);
+        // const languages = languagesObj.join(", ");
+
+        const countryData = {
+            commonName: countriesList[0].name.common,
+            capital: countriesList[0].capital[0],
+            population: countriesList[0].population,
+            flag: countriesList[0].flags.svg,
+            languages: Object.values(countriesList[0].languages).join(", "),
+        };
+
+        renderCountry(countryData);
+
+        console.log('One country ', countryData);
     } else if (countriesList.length > 1 && countriesList.length <= 10) {
         console.log('2 - 10 countries ');
-    } else if (countriesList.length > 10) {
-        console.log('too much ');
+    } else if (countriesList.length > 10) {        
+        console.log('Too many matches found. Please enter a more specific name.');
     };
+};
+
+const renderCountry = ({ commonName, capital, population, flag, languages}) => {
+    const markup = `
+    <div class="country-info__title-container">
+        <img src=${flag} width='40px'>
+        <h1 class="country-info__name">${commonName}<h1>
+    </div>
+    <ul class="country-info__list">
+        <li class="country-info__list-item"><span class="country-info__list-key">Capital:</span> ${capital}</li>
+        <li class="country-info__list-item"><span class="country-info__list-key">Population:</span> ${population}</li>
+        <li class="country-info__list-item"><span class="country-info__list-key">Languages:</span> ${languages}</li>
+    </ul>`
+
+    refs.countryInfo.innerHTML = markup;
 };
